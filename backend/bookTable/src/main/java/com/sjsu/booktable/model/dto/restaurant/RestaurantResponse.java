e87ea0adeeab73b8b108fa1adfc9647046359b41
Rajeev Ranjan Chaurasia <rajeevchaurasia.dev@gmail.com>
2025-05-05 01:28:32 -0700

    Revert "feat: implement admin functionality" (#19)

--- backend/bookTable/src/main/java/com/sjsu/booktable/model/dto/restaurant/RestaurantResponse.java
+++ backend/bookTable/src/main/java/com/sjsu/booktable/model/dto/restaurant/RestaurantResponse.java
@@ -9,16 +9,6 @@ public class RestaurantResponse {
 
     private int id;
     private String name;
-    private String cuisineType;
-    private int costRating;
-    private String description;
-    private String contactPhone;
-    private String addressLine;
-    private String city;
-    private String state;
-    private String zipCode;
-    private String country;
-    private String mainPhotoUrl;
     private boolean approved;
 
 }
