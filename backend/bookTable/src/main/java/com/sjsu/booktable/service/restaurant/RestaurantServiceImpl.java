5f7d21adc35c6ad3db0d23f0a786ca2ca9d6caff
Rajeev Ranjan Chaurasia <rajeevchaurasia.dev@gmail.com>
2025-05-09 14:17:41 -0700

    Added fetch Restaurant Details API and updated nearby restaurants API (#26)

--- backend/bookTable/src/main/java/com/sjsu/booktable/service/restaurant/RestaurantServiceImpl.java
+++ backend/bookTable/src/main/java/com/sjsu/booktable/service/restaurant/RestaurantServiceImpl.java
@@ -117,13 +117,13 @@ public class RestaurantServiceImpl implements RestaurantService {
                     searchRequest.getLongitude(), searchRequest.getLatitude(), searchRequest.getSearchText());
 
             List<RestaurantSearchDetails> availableRestaurants = new ArrayList<>();
+            LocalDate resDate = searchRequest.getDate();
+            int dayOfWeek = resDate.getDayOfWeek().getValue() % 7;
+            LocalTime resTime = searchRequest.getTime();
+            int partySize = searchRequest.getPartySize();
 
             for (RestaurantSearchDetails nearbyRestaurant : ListUtils.nullSafeList(nearbyRestaurants)) {
                 int restaurantId = nearbyRestaurant.getId();
-                LocalDate resDate = searchRequest.getDate();
-                int dayOfWeek = resDate.getDayOfWeek().getValue() % 7;
-                LocalTime resTime = searchRequest.getTime();
-                int partySize = searchRequest.getPartySize();
 
                 boolean withinHours = isWithinOperatingHours(restaurantId, resTime, dayOfWeek);
                 if (!withinHours) {
@@ -153,6 +153,52 @@ public class RestaurantServiceImpl implements RestaurantService {
         } catch (Exception e) {
             log.error("Error while searching for restaurants: ", e);
             throw e;
+        }
+    }
+
+    @Override
+    public RestaurantSearchResponse getNearbyRestaurants(NearbyRestaurantRequest request) {
+        try {
+            List<RestaurantSearchDetails> nearbyRestaurants = restaurantRepository.findNearbyRestaurants(
+                    request.getLongitude(), request.getLatitude(), request.getRadius());
+
+            List<RestaurantSearchDetails> availableRestaurants = new ArrayList<>();
+
+            LocalDate currentDate = LocalDate.now();
+            int dayOfWeek = currentDate.getDayOfWeek().getValue() % 7;
+            LocalTime currentTime = LocalTime.now();
+
+            for (RestaurantSearchDetails nearbyRestaurant : ListUtils.nullSafeList(nearbyRestaurants)) {
+                int restaurantId = nearbyRestaurant.getId();
+
+                boolean withinHours = isWithinOperatingHours(restaurantId, currentTime, dayOfWeek);
+                if (!withinHours) {
+                    continue;
+                }
+
+                List<LocalTime> matchingSlots = getMatchingTimeSlots(restaurantId, dayOfWeek, currentTime);
+
+                if (matchingSlots.isEmpty()) {
+                    continue;
+                }
+
+                List<String> availableTimeSlots = getAvailableTimeSlots(restaurantId, currentDate, matchingSlots, 1);
+
+                if (availableTimeSlots.isEmpty()) {
+                    continue;
+                }
+
+                nearbyRestaurant.setAvailableTimeSlots(availableTimeSlots);
+                availableRestaurants.add(nearbyRestaurant);
+            }
+
+            return RestaurantSearchResponse.builder()
+                    .count(availableRestaurants.size())
+                    .restaurantSearchDetails(availableRestaurants)
+                    .build();
+        } catch(Exception e){
+            log.error("Error while finding nearby restaurants: ", e);
+            throw new RestaurantException("Failed to find nearby restaurants. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -207,22 +253,6 @@ public class RestaurantServiceImpl implements RestaurantService {
                 .timeSlots(timeSlots)
                 .approved(restaurant.isApproved())
                 .build();
-    }
-
-    @Override
-    public RestaurantSearchResponse getNearbyRestaurants(NearbyRestaurantRequest request) {
-        try {
-            List<RestaurantSearchDetails> nearbyRestaurants = restaurantRepository.findNearbyRestaurants(
-                    request.getLongitude(), request.getLatitude(), request.getRadius());
-
-            return RestaurantSearchResponse.builder()
-                    .count(nearbyRestaurants.size())
-                    .restaurantSearchDetails(nearbyRestaurants)
-                    .build();
-        } catch (Exception e) {
-            log.error("Error while finding nearby restaurants: ", e);
-            throw new RestaurantException("Failed to find nearby restaurants. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     private int saveRestaurantDetails(RestaurantRequest request, double[] coords, String mainPhotoUrl, String managerId) {
